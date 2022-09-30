74dc0a2dd7d1120622478f00fce99242d1bac8ae
simplecloudservlet <outrosdiasvirao@gmail.com>
2022-09-30 10:44:58 -0300

    Initial commit.

---
+++ robotcloud/src/main/java/br/utfpr/edu/robotcloud/RobotcloudApplication.java
@@ -0,0 +1,13 @@
+package br.utfpr.edu.robotcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotcloudApplication.class, args);
+	}
+
+}
---
+++ robotcloud/src/test/java/br/utfpr/edu/robotcloud/RobotcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.utfpr.edu.robotcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
